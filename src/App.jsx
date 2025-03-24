1f4e9864f81dd2f1ba2b891d1cdc24612ebfacde
Lukas3162000 <Lukis3162000@aol.com>
2025-03-24 22:10:12 +0100

    Suchfunktion fertig, als nächstes URL search mit bot

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import"./App.css";
 
 function App() {
   const [users, setUsers] = useState([]); // Nutzerdaten aus der API
-  const [searchTerm, setSearchTerm] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
-  }, []);
+  }, [users]);
+
+  const eventHandler = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
 
   return (
     <div className="container">
       <h2>🔍 Benutzerliste</h2>
-      <input type="text" placeholder="Suche nach Namen..." />
+      <input type="text" placeholder="Suche nach Namen..." value={searchTerm} onChange={eventHandler} />
       <ul className="user-list">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
